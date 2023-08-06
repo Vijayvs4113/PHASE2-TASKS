4b28476f2833150048220ae33c3ea1804a9817ed
Vijay Subramaniyam <vijayvs6383@gmail.com>
2023-08-06 22:21:02 +0530

    NORMAL TASK

---
+++ portfolio website/script.js
@@ -0,0 +1,10 @@
+let submit = document.querySelector('.submit-button');
+
+submit.addEventListener('click',()=>{
+    let value1 = document.querySelector('.info');
+    value1.value = '';
+    let value2 = document.querySelector('.info1');
+    value2.value = '';
+    let value3 = document.querySelector('.Message');
+    value3.value = '';
+});
